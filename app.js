46884ba14d8bb2ea5c260e02b1f53c7f2d2a7486
Rohit Valiveti <rvv6@cornell.edu>
2022-04-16 23:45:41 -0400

    added other route handlers

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.send('<p>Hhefrwigreighierg<p>')
+  res.send('Home')
 })
+
+const students =[
+  {
+    netid: 'abc2',
+    year: 'freshman',
+    major: 'CS',
+    resume: 'resume'
+  },
+  {
+    netid: 'fht9',
+    year: 'senior',
+    major: 'IS',
+    resume: 'resume'
+  },
+
+]
 
 // Cornell Students
 app.get('/student/:netid', (req, res) => {
-  
+  const netid = req.params.netid
+  res.send(netid)
 })
 
+app.post('/student/:netid', (req, res) => {
+  console.log(req.body);
+  netid = req.params.netid
+  res.status(201).send(`Uploaded ${netid}'s Resume`)
+})
+
+app.delete('/student/:netid', (req, res) => {
+  const netid = req.params.netid
+})
 
 //Sponsers
 app.get('/sponser', (req, res) => {
-  
+  res.send(students)
 })
 
 
 
-//Admins
+//Admin
 app.get('/admin', (req, res) => {
-  res.send('<p>Admin<p>')
+  res.send('Admin Page')
 })
